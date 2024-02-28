0478cc0a36db299bcd76fa3cb1c45f8be657c99d
HelioJoseRR <heliojoseoficial@hotmail.com>
2024-02-28 13:50:53 -0300

    Update helio.c - rascunhos
    Teste de lista de prioridades

--- src/utils/helio.c
+++ src/utils/helio.c
@@ -1,74 +1,111 @@
-// Esboço para a fila de prioridade
+#include <stdio.h>
+#include <stdlib.h>
 
-/*
-    Fiz o código pensando nesse tipo de structs
-
-    typedef struct _huff
-    {
-        unsigned char byte;
-        int cont;
-        struct _huff *next;
-        struct _huff *left;
-        struct _huff *right;
-    } hufftree;
-
-    typedef struct temp_list
-    {
-        int size;
-        struct hufftree *head;
-    } temp_list;
-
-*/
-
-/*
-    Recebe um array com as frquencias de bytes e uma lista encadeada os nodes são do tipo hufftree e retorna a fila de prioridade
-    Assume que a lista encadeada já foi inicializada
-*/
-
-temp_list *create_priority_queue(temp_list *list, int **frequencies)
+typedef struct _huff
 {
+    unsigned char byte;
+    int cont;
+    struct _huff *next;
+    struct _huff *left;
+    struct _huff *right;
+} hufftree;
+
+typedef struct huff_list
+{
+    int size;
+    hufftree *head;
+} Hlist;
+
+int is_empty(Hlist *list)
+{
+    return list->size == 0 || list->head == NULL;
+}
+
+void enqueue_huff_list(Hlist *list, void *byte, int count, hufftree *left, hufftree *right)
+{
+    hufftree *new_huff = (hufftree *)malloc(sizeof(hufftree));
+    
+    new_huff->byte = *(unsigned char *)byte;
+    new_huff->cont = count;
+    new_huff->left = left;
+    new_huff->right = right;
+    new_huff->next = NULL;
+
+    if (is_empty(list))
+    {
+        list->head = new_huff;
+    }
+    else
+    {
+        hufftree *current = list->head;
+        hufftree *previous = NULL;
+
+        while (current != NULL && current->cont < new_huff->cont)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        if (previous == NULL)
+        {
+            new_huff->next = list->head;
+            list->head = new_huff;
+        }
+        else
+        {
+            previous->next = new_huff;
+            new_huff->next = current;
+        }
+    }
+
+    list->size++;
+
+    return;
+}
+
+Hlist *create_priority_queue(int *frequencies)
+{
+    Hlist *list = (Hlist *)malloc(sizeof(Hlist));
+
+    list->size = 0;
+    list->head = NULL;
+
     for (int i = 0; i < 256; i++)
     {
         if (frequencies[i] != 0)
         {
-            hufftree *new_huff = (hufftree *)malloc(sizeof(hufftree));
-            
-            new_huff->byte = i;
-            new_huff->cont = *frequencies[i];
-            new_huff->next = NULL;
-            new_huff->left = NULL;
-            new_huff->right = NULL;
-
-            if (list->head == NULL)
-            {
-                list->head = new_huff;
-            }
-            else
-            {
-                hufftree *current = list->head;
-                hufftree *previous = NULL;
-
-                while (current != NULL && current->cont < new_huff->cont)
-                {
-                    previous = current;
-                    current = current->next;
-                }
-
-                if (previous == NULL)
-                {
-                    new_huff->next = list->head;
-                    list->head = new_huff;
-                }
-                else
-                {
-                    previous->next = new_huff;
-                    new_huff->next = current;
-                }
-            }
-            
-            list->size++;
+            enqueue_huff_list(list, &i, frequencies[i], NULL, NULL);
         }
     }
-
+    
     return list;
+}
+
+void print_priority_queue(Hlist *list)
+{
+    hufftree *current = list->head;
+    
+    while (current != NULL)
+    {
+        printf("%c: %d\n", current->byte, current->cont);
+        current = current->next;
+    }
+
+    return;
+}
+
+int main()
+{
+    int frequencies[256];
+
+    for (int i = 0; i < 256; i++)
+    {
+        frequencies[i] = i;
+    }
+
+    Hlist *priority_queue = create_priority_queue(frequencies);
+
+    print_priority_queue(priority_queue);
+
+    return 0;
 }
