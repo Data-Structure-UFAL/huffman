106a4f685dee38aea862e6801d7a03f952c3a6e9
Josué Medino <josuemedino5@gmail.com>
2024-02-29 16:11:56 -0300

    modified space work

--- src/app/huffman.c
+++ src/app/huffman.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
+#include "adt.h"
+
 
 int main()
 {
--- src/branches/josue.c
+++ src/branches/josue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "utils.h"
+#include "./src/utils.h"
 
 struct node {
 	int priority;
