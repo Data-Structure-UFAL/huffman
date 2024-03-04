839fc8a90baf33db8542504b282bdf47548e21bb
Josué Medino <josuemedino5@gmail.com>
2024-03-04 09:44:06 -0300

    refactor: modularização e algumas alterações de logica visando um codigo mais limpo

--- src/branches/josue.c
+++ src/branches/josue.c
@@ -1,43 +1,52 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "./src/utils.h"
 
-struct node {
+
+typedef struct node {
 	int priority;
     unsigned char byte;
 	struct node *next;
     struct node *left;
     struct node *right;
-};
+} Node;
 
-struct queue {
-	struct node *head, *tail;
-};
+typedef struct {
+    Node * head;
+    int size;
+} Huff_Queue;
 
-struct queue * create_queue(){
-    struct queue * new_queue = malloc(sizeof(struct queue));
-    new_queue->head = new_queue->tail = NULL;
+
+Node * create_node(unsigned char byte, int frequency){
+    Node *new_node = malloc(sizeof(Node));
+	new_node->priority = frequency;
+    new_node->byte = byte;
+    return new_node;
+}
+
+Huff_Queue * create_queue(){
+    Huff_Queue * new_queue = malloc(sizeof(Huff_Queue));
+    new_queue->head = NULL;
+    new_queue->size = 0;
     return new_queue;
 }
 
-int is_empty_queue(struct queue * queue){
+int is_empty_queue(Huff_Queue * queue){
     return queue->head == NULL;
 }
 
-void priority_queue(struct queue * queue, int item){
-	struct node *new_node = malloc(sizeof(struct node));
-	new_node->priority = item;
+void enqueue_priority(Huff_Queue * queue, int freq, unsigned char byte){
+    Node * new_node = create_node(byte, freq);
 
-    if(is_empty_queue(queue)|| (item > queue->head->priority) ){
+    if(is_empty_queue(queue) || (freq < queue->head->priority) ){
         new_node->next = queue->head;
         queue->head = new_node;
         return;
     } 
 
-    struct node * current = queue->head;
+    Node * current = queue->head;
 
-    while (current->next != NULL && current->next->priority > item)
+    while (current->next != NULL &&  freq > current->next->priority)
     {
         current = current->next;
     }
@@ -46,37 +55,17 @@ void priority_queue(struct queue * queue, int item){
     current->next = new_node;
 }
 
-void enqueue(struct queue * queue, int item, unsigned char byte){
-
-    struct node *new_node = malloc(sizeof(struct node));
-	new_node->priority = item;
-    new_node->byte = byte;
-
-    if(is_empty_queue(queue)){
-        new_node->next = queue->head;
-        queue->head = new_node;
-        queue->tail = new_node;
-        return;
-    }
-
-    queue->tail->next = new_node;
-    queue->tail = new_node;
-    new_node->next = queue->tail;
-
-    queue->tail = new_node;
-    queue->tail->next = NULL;
-}
 
 
 int main(){
-   struct queue * queue = create_queue();
+    Huff_Queue * queue = create_queue();
 
-    enqueue(queue, 30, 'd');
-    enqueue(queue, 20, 'a');
-    enqueue(queue, 10, 'z');
-    enqueue(queue, 12, 'j');
-    enqueue(queue, 1, 'h');
-    enqueue(queue, 100, 'x');
+    enqueue_priority(queue, 30, 'd');
+    enqueue_priority(queue, 20, 'a');
+    enqueue_priority(queue, 10, 'z');
+    enqueue_priority(queue, 12, 'j');
+    enqueue_priority(queue, 1, 'h');
+    enqueue_priority(queue, 100, 'x');
 
     struct node * current = queue->head;
     while (current != NULL)
