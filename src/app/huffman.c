98d3e50092d3c2c9cc27a649523b91a2418ebcdb
Josué Medino <josuemedino5@gmail.com>
2024-03-11 00:15:38 -0300

    feat: initial logic in main code

--- src/app/huffman.c
+++ src/app/huffman.c
@@ -16,6 +16,40 @@ int main()
     {
     case 1:
         printf("Comprindo arquivo: %s\n", path);
+        
+        /* ### PROCESSO DE COMPRESSÃO ### */
+
+        /* Ler arquivo em binário */
+        object_data * binary_data= read_initial_file(path);
+        printf("\tbyte: %s\n\tsize: %ld\n", binary_data->byte, binary_data->size);
+
+        /* Criar lista de frequencia */
+        int arr_freq[ASCII_LENGTH];
+        get_frequency(binary_data, arr_freq);
+        
+        /* Cria lista de prioridade */
+        Huff_Queue * queue = create_queue();
+
+        for (int i = 0; i < ASCII_LENGTH; i++)
+        {
+           if(arr_freq[i] > 0){
+            Node * new_node = create_node(i, arr_freq[i]);
+            enqueue_priority(queue, new_node);
+           }
+        }
+
+        Node * current = queue->head;
+        while (current != NULL)
+        {
+            printf("\tc: %c  -  f: %d\n", current->byte, current->priority );
+            current = current->next;
+        }
+        
+        /* Criar arvore de huffman */
+        Huff_Tree * huff_tree = create_huffman_tree(queue);
+
+        
+
         break;
 
     case 2:
