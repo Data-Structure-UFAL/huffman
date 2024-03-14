39abfe05262bf8a97647fc2a6f8cf955fbb97068
HelioJoseRR <hjrr@ic.ufal.br>
2024-03-14 06:52:05 -0300

    Update helio.c
    add adt.h to void

--- src/branches/helio.c
+++ src/branches/helio.c
@@ -1,124 +1,80 @@
+#ifndef ADT_H
+#define ADT_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
 
 #define ASCII_LENGTH 256
 
-typedef struct node
-{
-    void *byte;
+typedef struct node {
+	void *byte;
     void *priority;
     struct node *next;
     struct node *left;
     struct node *right;
 } Node;
 
-typedef struct
-{
+typedef struct {
     Node *head;
     void *size;
 } Huff_Queue;
 
-typedef struct
-{
-    Node *root;
+typedef struct {
+    Node * root;
 } Huff_Tree;
 
 /*
-    # OBJETIVO: Criar um nó
-    # PARAMETRO: É passado dois parâmetro, um informando o byte e outro a frequência daquele byte
-    # RETORNO: É retornado um nó com as informações passadas
+    - OBJETIVO: Criar um nó
+    - PARAMETRO: É passado dois parâmetros, um informando o byte e outro a frequência daquele byte
+    - RETORNO: É retornado um nó com as informações passadas
 */
 Node *create_node(void *byte, void *frequency);
 
 /*
-    # OBJETIVO: Criar uma lista
-    # PARAMETRO: Não tem parâmetro
-    # RETORNO: É retornado o head da lista apontando para NULL
+    - OBJETIVO: Criar uma lista 
+    - PARAMETRO: Não tem parâmetro
+    - RETORNO: É retornado o head da lista apontando para NULL
 */
-Huff_Queue *create_queue();
+Huff_Queue * create_queue();
 
 /*
-    # OBJETIVO: Verificar se a lista de prioridade está vazia
-    # PARAMETRO: É passado um ponteiro para a lista de prioridade
-    # RETORNO: É retornado 1 se a lista estiver vazia e 0 caso contrário
+    - OBJETIVO: Verificar se a lista de prioridade está vazia
+    - PARAMETRO: É passado um ponteiro para a lista de prioridade
+    - RETORNO: É retornado 1 se a lista estiver vazia e 0 caso contrário
 */
-int is_empty_queue(Huff_Queue *queue);
+int is_empty_queue(Huff_Queue * queue);
 
 /*
-    # OBJETIVO: Adicionar de forma ordenada um novo nó na lista de prioridade
-    # PARAMETRO: É passado um ponteiro para a lista de prioridade, e  o nó que deve ser adicionado
-    # RETORNO: Não é retornado nada, mas é adicionado um novo node na lista de prioridade
+    - OBJETIVO: Adicionar de forma ordenada um novo nó na lista de prioridade
+    - PARAMETRO: É passado um ponteiro para a lista de prioridade, e  o nó que deve ser adicionado
+    - RETORNO: Não é retornado nada, mas é adicionado um novo node na lista de prioridade
 */
-void enqueue_priority(Huff_Queue *queue, Node *new_node);
+void enqueue_priority(Huff_Queue * queue, Node * new_node);
 
 /*
-    # OBJETIVO: Remover o head de uma Lista
-    # PARAMETRO: É passado a Lista como parâmetro
-    # RETORNO: É retornado o nó removido
+    - OBJETIVO: Remover o head de uma Lista
+    - PARAMETRO: É passado a Lista como parâmetro
+    - RETORNO: É retornado o nó removido
 */
 
-Node *dequeue(Huff_Queue *queue);
+Node * dequeue(Huff_Queue * queue);
 
 /*
-    # OBJETIVO: Criar uma árvore de huffman
-    # PARAMETRO: É passado a Lista como parâmetro
-    # RETORNO: É retornado um nó representando o a Raiz da arvore montada
+    - OBJETIVO: Criar uma árvore de huffman
+    - PARAMETRO: É passado a Lista como parâmetro
+    - RETORNO: É retornado um nó representando o a Raiz da arvore montada
 */
-Node *create_huffman_tree(Huff_Queue *queue);
+Node * create_huffman_tree(Huff_Queue * queue);
 
-/*
-    # OBJETIVO: Criar uma lista de prioridade
-    # PARAMETRO: É passado um array de frequências e a lista de prioridade
-    # RETORNO: Não é retornado nada, mas é adicionado um novo node na lista de prioridade
+
+
+/* 
+- Objetivo: Alocar e instanciar um No
+- Parametro: Um Byte e a Frequencia do byte
+- Retorno: Um No alocado e instanciado  
 */
-
-void create_priority_queue(int frequencies[], Huff_Queue *queue);
-
-// testar // ==========================================================================
-void print_in_order(Node *root);
-
-void print_queue(Huff_Queue *queue);
-
-// =====================================================================================
-
-int main()
-{
-    Huff_Queue *queue = create_queue();
-
-    int freq[ASCII_LENGTH] = {0};
-
-    char *text = "çopçsxassasasd~";
-
-    int tam = strlen(text);
-
-    for (int i = 0; i < tam; i++)
-    {
-        freq[(unsigned char)text[i]]++;
-    }
-
-    for (int i = 0; i < ASCII_LENGTH; i++)
-    {
-        if (freq[i] > 0)
-        {
-            Node *new_node = create_node(&i, &freq[i]);
-
-            enqueue_priority(queue, new_node);
-        }
-    }
-
-    print_queue(queue);
-
-    Node *root = create_huffman_tree(queue);
-
-    print_in_order(root);
-
-    return 0;
-}
-
-Node *create_node(void *byte, void* frequency) 
-{
+Node *create_node(void *byte, void* frequency) {
     Node *new_node = (Node *)malloc(sizeof(Node));
 
     int *priority = (int *)malloc(sizeof(int));
@@ -129,42 +85,49 @@ Node *create_node(void *byte, void* frequency)
 
     new_node->priority = priority;
     new_node->byte = new_byte;
-    
     new_node->left = NULL;
     new_node->right = NULL;
 
     return new_node;
 }
 
-Huff_Queue *create_queue()
-{
-    Huff_Queue *new_queue = (Huff_Queue *)malloc(sizeof(Huff_Queue));
-
-    new_queue->head = NULL;
-
+/* 
+- Objetivo: Instancia uma lista 
+- Parametro: Sem parametros
+- Retorno: Um struct contendo um ponteiro para o inicio da lista e o tamanho atual 
+*/
+Huff_Queue * create_queue(){
+    Huff_Queue * new_queue = malloc(sizeof(Huff_Queue));
     int *size = (int *)malloc(sizeof(int));
-    *size = 0;
-    new_queue->size = size;
 
+    *size = 0;
+    new_queue->head = NULL;
+    new_queue->size = size;
     return new_queue;
 }
 
-int is_empty_queue(Huff_Queue *queue)
-{
+/* 
+- Objetivo: Verificar se a lista esta vazia
+- Parametro: Uma Lista
+- Retorno: Um inteiro representando true ou false
+*/
+int is_empty_queue(Huff_Queue * queue){
     return queue->head == NULL;
 }
 
-void enqueue_priority(Huff_Queue *queue, Node *new_node)
-{
-    if (is_empty_queue(queue) || (*(int *)new_node->priority < *(int *)queue->head->priority) ||
-        (*(int *)new_node->priority == *(int *)queue->head->priority && *(unsigned char *)new_node->byte < *(unsigned char *)queue->head->byte))
-    {
+/* 
+- Objetivo: Adicionar um no na lista de forma crescente
+- Parametro: Lista,e o No a ser adicionado
+- Retorno: Sem retorno, a lista eh alterada por referencia
+*/
+void enqueue_priority(Huff_Queue * queue, Node * new_node){
+   /*  Node * new_node = create_node(byte, freq); */
 
+    if (is_empty_queue(queue) || (*(int *)new_node->priority < *(int *)queue->head->priority) ||
+        (*(int *)new_node->priority == *(int *)queue->head->priority && *(unsigned char *)new_node->byte < *(unsigned char *)queue->head->byte)) {
         new_node->next = queue->head;
         queue->head = new_node;
-
-        *(int *)(queue->size) += 1;
-
+        *(int *)queue->size += 1;
         return;
     }
 
@@ -172,27 +135,29 @@ void enqueue_priority(Huff_Queue *queue, Node *new_node)
 
     while (current->next != NULL &&
            (*(int *)new_node->priority > *(int *)current->next->priority ||
-            (*(int *)new_node->priority == *(int *)current->next->priority && *(unsigned char *)new_node->byte > *(unsigned char *)current->next->byte)))
-    {
+            (*(int *)new_node->priority == *(int *)current->next->priority && *(unsigned char *)new_node->byte > *(unsigned char *)current->next->byte))) {
         current = current->next;
     }
 
     new_node->next = current->next;
     current->next = new_node;
 
-    *(int *)(queue->size) += 1;
+    *(int *)queue->size += 1;
 }
 
 /* CREATE TREE HUFFMAN */
-Node *dequeue(Huff_Queue *queue)
-{
-    if (is_empty_queue(queue))
-    {
+/* 
+- Objetivo: Remover o primeiro no da lista
+- Parametro: Uma lista
+- Retorno: O no removido 
+*/
+Node * dequeue(Huff_Queue * queue){
+    if(is_empty_queue(queue)){
         printf("Queue Underflow\n");
         abort();
     }
 
-    Node *dequeued = queue->head;
+    Node * dequeued = queue->head;
 
     queue->head = queue->head->next;
     dequeued->next = NULL;
@@ -202,8 +167,12 @@ Node *dequeue(Huff_Queue *queue)
     return dequeued;
 }
 
-Node *create_huffman_tree(Huff_Queue *queue)
-{
+/* 
+- Objetivo: Construir a arvore de huffman
+- Parametro: Um no apontando para o inicio de uma lista de prioridades
+- Retorno: Um no apontando para a raiz da arvore
+*/
+Node * create_huffman_tree(Huff_Queue * queue){
     Node *left, *right, *new_node;
 
     while (*(int *)queue->size > 1)
@@ -215,7 +184,6 @@ Node *create_huffman_tree(Huff_Queue *queue)
         int sum_priority = *(int *)left->priority + *(int *)right->priority;
 
         new_node = create_node(&asterisco, &sum_priority);
-
         new_node->left = left;
         new_node->right = right;
 
@@ -225,37 +193,226 @@ Node *create_huffman_tree(Huff_Queue *queue)
     return dequeue(queue);
 }
 
-void create_priority_queue(int frequencies[], Huff_Queue *queue)
-{
-    for (int i = 0; i < ASCII_LENGTH; i++)
+/* 
+- Objetivo: Calcular a altura da arvore
+- Parametro: Um no apontando para a raiz da arvore
+- Retorno: Um inteiro representando a altura
+*/
+int tree_height(Node * root){
+    if(root == NULL) return -1;
+
+    int left = tree_height(root->left);
+    int right = tree_height(root->right);
+
+    return (left > right) ? left + 1: right + 1; 
+}
+
+/* 
+- Objetivo: Fazer alocação de memória do dicionário
+- Parametro: Um inteiro representndo o numero de colunas
+- Retorno: Um ponteiro que aponta para um array de strings
+*/
+char ** create_empty_dictionary(int column){ /* Remember to use CONST LENGTH_ASCII and not 256*/
+    char **dictionary;
+    dictionary = malloc(sizeof(char*) * 256); 
+
+    for (int i = 0; i < 256; i++)
+        dictionary[i] =  calloc(column, sizeof(char));
+
+    return dictionary;
+}
+
+/* 
+- Objetivo: Criar um dicionário que referencia o caminho das folhas de um byte 
+- Parametro: Dicionario Alocado, Raiz, Caminho Vazio, Numero de Colunas
+- Retorno: Sem retorno... altera o dicionário por referencia
+*/
+void create_dictionary(char **dictionary, Node *root, char *path, int column) {
+    char left[column], right[column];
+
+    if (root->left == NULL && root->right == NULL) {
+        strcpy(dictionary[*(unsigned char *)root->byte], path);
+    } else {
+        strcpy(left, path);
+        strcpy(right, path);
+
+        strcat(left, "0");
+        strcat(right, "1");
+
+        create_dictionary(dictionary, root->left, left, column);
+        create_dictionary(dictionary, root->right, right, column);
+    }
+}
+
+
+void print_dictionary(char ** dictionary){
+    for (int i = 0; i < 256; i++)
     {
-        if (frequencies[i] > 0)
+        if(strlen(dictionary[i]) > 0)
+            printf("\t%3c: %s\n", i, dictionary[i]);
+    }
+    
+}
+
+void print_huff_tree(Node * huff_tree, int heigth){
+    if(huff_tree->left == NULL && huff_tree->right == NULL){
+        printf("leaf: %c - height: %d\n", *(unsigned char *)huff_tree->byte ,heigth);
+        return;
+    }
+
+    print_huff_tree(huff_tree->left, heigth + 1);
+    print_huff_tree(huff_tree->right, heigth + 1);
+}
+
+
+/* CODIFICAÇÃO DO TEXTO */
+
+/* 
+- Objetivo: Calcular o numero de bits com referencia ao dicionario
+- Parametro: Dicionario, Texto
+- Retorno: Um inteiro representando o numero de bits
+*/
+int size_text_coding(char ** dictionary, unsigned char * text){
+    int length = 0, i = 0;
+
+    while (text[i] != '\0')
+    {
+        length += strlen(dictionary[text[i]]);
+        i++;
+    }
+    return length + 1;
+}
+/* 
+- Objetivo: Codificar um texto em bits (formato de texto) com base no dicionario
+- Parametro: Dicionario, Texto
+- Retorno: Uma string de um texto codificado
+*/
+char * coding_text(char ** dictionary, unsigned char * text){
+    int i = 0, size =  size_text_coding(dictionary, text);
+    printf("\tsize_text: %d\n", size);
+    char * code = calloc(size, sizeof(char));
+
+    while (text[i] != '\0')
+    {
+        strcat(code, dictionary[text[i]]);
+        i++;
+    }
+    
+    return code;
+    
+}
+
+/* DECODING JUST TEXT TO LERANING */
+
+/* 
+- Objetivo: Decodificar uma sequencia de bits (char) no texto original
+- Parametro: Texto codificado, Raiz da Arvore de  Huffman
+- Retorno: Uma string decodificada 
+*/
+unsigned char * decoding_text(char text[], Node * root){
+    int i = 0;
+    Node * aux = root;
+    char temp[2];
+    unsigned char * decoding = calloc(strlen(text) , sizeof(unsigned char));
+
+
+    while (text[i] != '\0')
+    {
+       
+        if(text[i] == '0')
+            aux = aux->left;
+        else     
+            aux = aux->right;
+
+        if (aux->left == NULL && aux->right == NULL)
         {
-            enqueue_priority(queue, create_node(&i, &frequencies[i]));
+            temp[0] = *(unsigned char *)aux->byte;
+            temp[1] = '\0';
+
+            strcat(decoding, temp);
+            aux = root;
+        }
+
+        i++;
+    }
+
+    return decoding;
+}
+
+/* CODING IN ARCHIVE */
+void coding(char * text_coded){
+    FILE * file = fopen("compress.medino", "wb");
+
+    if(!file){
+        printf("Problema ao abrir arquivo em Coding()\n");
+        return;
+    }
+
+    int i = 0, j = 7;
+    unsigned char mask, byte = 0;
+
+    while (text_coded[i] != '\0')
+    {
+        mask = 1;
+        if(text_coded[i] == '1'){
+            mask = mask << j;
+            byte = byte | mask;
+        }
+        j--;
+
+        if(j < 0){
+            fwrite(&byte, sizeof(unsigned char), 1, file);
+            byte = 0;
+            j = 7;
+        }
+        i++;
+    }
+    
+    if(j != 7) fwrite(&byte, sizeof(unsigned char), 1, file);
+
+
+    fclose(file);
+}
+
+unsigned int is_on_bit(unsigned char byte, int i){
+    unsigned char mask = (1 <<i);
+    return byte & mask;
+}
+
+void decoding(Node * root){
+    FILE * file = fopen("compress.medino", "rb");
+
+    if(!file){
+        printf("Problema ao abrir arquivo em Decoding()\n");
+        return;
+    }
+
+    unsigned char byte; 
+    Node * current = root;
+    while (fread(&byte, sizeof(unsigned char), 1, file))
+    {
+        for (int i = 7; i > - 0; i--)        
+        {
+            current = (is_on_bit(byte, i)) ? current->right : current->left;
+
+            if(current->left == NULL && current->right == NULL){
+                printf("decoding:%c\n", *(unsigned char *)current->byte);
+                current = root;
+            }
         }
     }
+
+    fclose(file);
 }
 
 
-//==================================================================================================================
+//FUNCAO DE TESTE PARA FINS DE TESTE
 
-void print_queue(Huff_Queue *queue)
+int teste(int i)
 {
-    Node *current = queue->head;
+    if(i == 1) return 0;
 
-    while (current != NULL)
-    {
-        printf("%c %d\n", *(unsigned char *)current->byte, *(int *)current->priority);
-        current = current->next;
-    }
+    return 1;
 }
 
-void print_in_order(Node *root)
-{
-    if (root != NULL)
-    {
-        print_in_order(root->left);
-        printf("%c %d\n", *(unsigned char *)root->byte, *(int *)root->priority);
-        print_in_order(root->right);
-    }
-}
+#endif
