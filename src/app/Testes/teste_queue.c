f5214dfa02af50d0580b98cde89823422b775e17
davi <davimelovas61@gmail.com>
2024-03-13 23:57:20 -0300

    incrementando os testes

--- src/app/Testes/teste_queue.c
+++ src/app/Testes/teste_queue.c
@@ -20,20 +20,50 @@ void test_funcao_is_empty_false()
 
     Huff_Queue *queue = create_queue();
 
-    Node *new_node = create_node(65, 30);
+    Node *new_node = create_node('a', 30);
 
     enqueue_priority(queue, new_node);
 
     TEST_ASSERT_EQUAL(0, is_empty_queue(queue));
 }
 
-int main()
+int testando_funcao_is_empty()
 {
-
     UNITY_BEGIN();
 
     RUN_TEST(test_funcao_is_empty_true);
     RUN_TEST(test_funcao_is_empty_false);
 
     return UNITY_END();
+}
+
+
+
+int main()
+{
+    int opcao;
+
+    puts("Selecione a opção que você gostaria de testar");
+    puts("1 - Is_Empty");
+    puts("2 - Enqueue_Priority");
+    puts("3 - Dequeue");
+
+    scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+        case 1:
+            testando_funcao_is_empty();
+            break;
+        case 2:
+
+            break;
+        case 3:
+            
+            break;
+        default:
+            puts("Opcao Invalida.");
+            break;
+    }
+    
 }
